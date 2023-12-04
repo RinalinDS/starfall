1aedf92b95e9ed8ae0f874c1ad6b084012fa2fdc
DenisPilyutin <denispilyutin@gmail.com>
2023-12-05 00:58:39 +0200

    feat: add ShadowButton to Ui-Kit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import {Search,} from "./UI-kit/Search/Search.tsx";
+import styled, { ThemeProvider } from "styled-components";
+import {Search} from "./UI-kit/Search/Search.tsx";
+import { ShadowButton } from "./UI-kit/ShadowButton/ShadowButton.tsx";
+
+
+const theme = {
+  baseColor: '#3498db',
+  secondBaseColor: '#002366',
+  royalBlue: '#4169e1',
+};
 
 export const App = () => {
 
   return (
-    <>
-      <Search onClick={()=> {}}/>
-    </>)
+    <ThemeProvider theme={theme}>
+      <Container>
+       <Search onClick={()=> {}}/>
+        <ShadowButton onClick={()=>{}}>Helloadfadfad</ShadowButton>
+      </Container>
+    </ThemeProvider>)
 }
 
+const Container = styled.div`
+  max-width: 144rem;
+  margin: 0 auto;
+  padding: 4.8rem 8.4rem;
+  background-color: #bbb;
+`
