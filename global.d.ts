e3dcf5cc62b5983d071a359b65ff6dd2bfebee99
DenisPilyutin <denispilyutin@gmail.com>
2024-03-21 01:25:46 +0200

    feat: further setup vitest

---
+++ global.d.ts
@@ -0,0 +1,12 @@
+// import '@testing-library/jest-dom';
+
+// declare global {
+//   namespace jest {
+//     interface Matchers<R> {
+//       toBeInTheDocument(): R;
+//       // Add other custom matchers here as needed
+//     }
+//   }
+// }
+
+// problems with toBeInTheDocument created by    "@testing-library/jest-dom" every version after 6 , rn i have 5.17 , if i will upadte , i should consider uncomment this doc
