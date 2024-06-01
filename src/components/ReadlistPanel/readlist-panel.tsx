6d36b69f69fc8f5ff137eaaf50b59a86cac45ccb
DenisPilyutin <denispilyutin@gmail.com>
2024-06-01 15:29:10 +0300

    feat: add icon !

--- src/components/ReadlistPanel/readlist-panel.tsx
+++ src/components/ReadlistPanel/readlist-panel.tsx
@@ -1,5 +1,6 @@
 import { styled } from 'styled-components';
 import RightArrow from './../../assets/rightarrow.svg?react';
+import { BsFillBookmarkPlusFill } from 'react-icons/bs';
 
 export const ReadlistPanel = () => {
   return (
@@ -9,12 +10,14 @@ export const ReadlistPanel = () => {
           From your Readlist <RightArrow className="icon" />
         </a>
       </Title>
-      <div>
-        <div>icon</div>
+      <ContentContainer>
+        <SvgContainer>
+          <BsFillBookmarkPlusFill fontSize={'36px'} className="icon" />
+        </SvgContainer>
         <div>your watchlist is emopty</div>
         <div>Save books to keep track of what you want to read</div>
         <button> browse popular movies</button>
-      </div>
+      </ContentContainer>
     </Container>
   );
 };
@@ -22,9 +25,44 @@ export const ReadlistPanel = () => {
 const Container = styled.div`
   padding: 4.8rem 0;
   font-size: 1.6rem;
+  position: relative;
+`;
+
+const SvgContainer = styled.div`
+  position: relative;
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    top: 10%;
+    left: 30%;
+    height: 60%;
+    width: 50%;
+    background-color: ${({ theme }) => theme.text.fill};
+  }
+  & .icon {
+    position: relative;
+    z-index: 2;
+    fill: ${({ theme }) => theme.text.stroke};
+  }
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.6rem;
+  margin-top: 2.4rem;
+  align-items: center;
+
+  color: ${({ theme }) => theme.text.primary};
+
+  > :last-child {
+    margin-top: 2rem;
+  }
 `;
 
 const Title = styled.h3`
+  display: inline-block;
   position: relative;
   color: ${({ theme }) => theme.text.primary};
   & .link,
