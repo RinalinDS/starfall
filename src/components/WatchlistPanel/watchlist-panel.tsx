527ac216422927f6fc630842a3e0dfdc374e15df
DenisPilyutin <denispilyutin@gmail.com>
2024-06-01 11:47:16 +0300

    feat: watchlist panel updates

--- src/components/WatchlistPanel/watchlist-panel.tsx
+++ src/components/WatchlistPanel/watchlist-panel.tsx
@@ -1,3 +1,37 @@
+import { styled } from 'styled-components';
+import RightArrow from './../../assets/rightarrow.svg?react';
+
 export const WatchlistPanel = () => {
-  return <div>watchlist-panel</div>;
+  return (
+    <Container>
+      <Title>
+        From Your Readlist <RightArrow />
+      </Title>
+      <div>
+        <div>icon</div>
+        <div>your watchlist is emopty</div>
+        <div>Save books to keep track of what you want to read</div>
+        <button> browse popular movies</button>
+      </div>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  padding: 4.8rem 0;
+  font-size: 1.6rem;
+`;
+
+const Title = styled.h3`
+  position: relative;
+  &::before {
+    background: #f5c518;
+    border-radius: 4px;
+    content: '';
+    margin-left: -1.5rem;
+    align-self: flex-start;
+    position: absolute;
+    height: 100%;
+    width: 4px;
+  }
+`;
