8ef6e7f5de6ff587150bdd83bc0d79bfc6ba8a47
DenisPilyutin <denispilyutin@gmail.com>
2024-06-23 17:29:22 +0300

    feat: typography component

--- src/UI-kit/Typography/Typography.tsx
+++ src/UI-kit/Typography/Typography.tsx
@@ -1,11 +1,35 @@
-export const Typography = <C extends React.ElementType>({
+import React from 'react';
+import s from './typography.module.css';
+
+type Variant = 'body1' | 'body2' | 'italic' | 'bold' | 'error' | 'highlighted';
+
+type TypographyProps = {
+  className?: string;
+  variant?: Variant;
+};
+
+type AsProp<T extends React.ElementType> = { as?: T };
+
+type PropsToOmit<T extends React.ElementType, P> = keyof (AsProp<T> & P);
+
+type PolymorphicComponentProps<
+  T extends React.ElementType,
+  P,
+> = React.PropsWithChildren<P & AsProp<T>> &
+  Omit<React.ComponentPropsWithoutRef<T>, PropsToOmit<T, P>>;
+
+export const Typography = <T extends React.ElementType = 'span'>({
   as,
   children,
-}: {
-  as?: C;
-  children: React.ReactNode;
-}) => {
-  const Component = as || 'span';
-
-  return <Component>{children}</Component>;
+  className,
+  variant = 'body1',
+  ...rest
+}: PolymorphicComponentProps<T, TypographyProps>) => {
+  const Tag = as || 'span';
+  const classNameComputed = `${s[variant]} ${className}`;
+  return (
+    <Tag {...rest} className={classNameComputed}>
+      {children}
+    </Tag>
+  );
 };
