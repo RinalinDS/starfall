df529504bb8745245f31b65a4c189eec840baa62
DenisPilyutin <denispilyutin@gmail.com>
2025-04-15 22:08:37 +0300

    feat: add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), TanStackRouterVite()],
+  plugins: [react(), svgr(), TanStackRouterVite(), tailwindcss()],
   server: {
     fs: {
       cachedChecks: false,
