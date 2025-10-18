abffb46186eaa90bd737b0eefd62825c082f9a0d
DenisPilyutin <denispilyutin@gmail.com>
2025-10-18 21:43:11 +0300

    fix: card max-width

--- src/components/Card/atoms/CardContainer.tsx
+++ src/components/Card/atoms/CardContainer.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 export const CardContainer = ({ children }: { children: ReactNode }) => {
   return (
     <div
-      className={`flex flex-col overflow-hidden rounded-md bg-gray-300 text-gray-900 dark:bg-gray-700 dark:text-gray-50`}
+      className={`flex max-w-[200px] flex-col overflow-hidden rounded-md bg-gray-300 text-gray-900 dark:bg-gray-700 dark:text-gray-50`}
     >
       {children}
     </div>
